5be987744d0f52669853599380ac5a22b00258ef
Carsmo <lche3@wisc.edu>
2024-09-06 12:09:07 -0500

    add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -6,11 +6,10 @@ import { getStorage, getDownloadURL, ref, uploadBytes, uploadBytesResumable } fr
 import { app } from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../../redux/user/userSlice'
+import { updateStart, updateSuccess, updateFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signoutSuccess } from '../../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 import { Modal } from 'flowbite-react'
 import { HiOutlineExclamationCircle } from 'react-icons/hi'
-
 
 export default function DashProfile() {
     const {currentUser, error} = useSelector(state => state.user)
@@ -126,6 +125,23 @@ export default function DashProfile() {
             dispatch(deleteUserFailure(error.message));
         }
     }
+
+    const handleSignout = async () =>{
+        try{
+            const res = await fetch('/api/user/signout', {
+                method: 'POST',
+            });
+            const data = await res.json
+            if(!res.ok){
+                console.log(data.message);
+            }
+            else{
+                dispatch(signoutSuccess());
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
@@ -157,7 +173,7 @@ export default function DashProfile() {
         </form>
         <div className='text-red-500 flex justify-between mt-5'>
             <span onClick={() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-            <span className='cursor-pointer'>Sign Out</span>
+            <span onClick = {handleSignout} className='cursor-pointer'>Sign Out</span>
         </div>
         {updateUserSuccess && (
             <Alert color='success' className='mt-5'>
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -3,17 +3,38 @@ import React, {useEffect, useState} from 'react'
 import { useLocation } from 'react-router-dom'
 import { HiArrowSmRight, HiUser } from 'react-icons/hi'
 import { Link } from 'react-router-dom'
+import { signoutSuccess } from '../../redux/user/userSlice'
+import { useDispatch } from 'react-redux';
 
 export default function DashSidebar() {
     const location = useLocation()
     const [tab, setTab] = useState('')
+    const dispatch = useDispatch();
     useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
     const tabFromUrl = urlParams.get('tab')
-    if(tabFromUrl){
-      setTab(tabFromUrl)
+      if(tabFromUrl){
+        setTab(tabFromUrl)
+      }
+    }, [location.search]);
+
+  const handleSignout = async () =>{
+    try{
+        const res = await fetch('/api/user/signout', {
+            method: 'POST',
+        });
+        const data = await res.json
+        if(!res.ok){
+            console.log(data.message);
+        }
+        else{
+            dispatch(signoutSuccess());
+        }
+    }catch(error){
+        console.log(error);
     }
-  }, [location.search]);
+}
+
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
@@ -21,7 +42,7 @@ export default function DashSidebar() {
                 <Link to='/dashboard?tab=profile'>
                 <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>Profile</Sidebar.Item>
                 </Link>
-                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
+                <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
             </Sidebar.ItemGroup>
         </Sidebar.Items>
     </Sidebar>
