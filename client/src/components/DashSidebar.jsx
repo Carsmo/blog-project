b3a1b8405c04c3ecfbd7805fbb8324cc51fd9e4b
Carsmo <lche3@wisc.edu>
2024-09-07 12:43:03 -0500

    add posts section

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,14 +1,16 @@
 import { Sidebar } from 'flowbite-react'
 import React, {useEffect, useState} from 'react'
 import { useLocation } from 'react-router-dom'
-import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi'
 import { Link } from 'react-router-dom'
 import { signoutSuccess } from '../../redux/user/userSlice'
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux'
 
 export default function DashSidebar() {
-    const location = useLocation()
-    const [tab, setTab] = useState('')
+    const currentUser = useSelector(state => state.user.currentUser);
+    const location = useLocation();
+    const [tab, setTab] = useState('');
     const dispatch = useDispatch();
     useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
@@ -38,10 +40,17 @@ export default function DashSidebar() {
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
-            <Sidebar.ItemGroup>
+            <Sidebar.ItemGroup className='flex flex-col gap-1'>
                 <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>Profile</Sidebar.Item>
+                <Sidebar.Item as='div' active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin? 'Admin': 'User'} labelColor='dark'>Profile</Sidebar.Item>
                 </Link>
+                {currentUser.isAdmin && (
+                <Link to='/dashboard?tab=posts'>
+                  <Sidebar.Item active={tab==='posts'} icon={HiDocumentText} as='div'>
+                    Posts
+                  </Sidebar.Item>
+                </Link>
+                )}
                 <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} className='cursor-pointer'>Sign Out</Sidebar.Item>
             </Sidebar.ItemGroup>
         </Sidebar.Items>
