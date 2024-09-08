34c4e00f642a38b95b1c4cec74f87b9dfabe0e8d
Carsmo <lche3@wisc.edu>
2024-09-07 21:30:55 -0500

    add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -51,9 +51,18 @@ export default function DashUsers() {
 
     const handleDeleteUser = async () => {
         try{
-
+            const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if(res.ok){
+                setUsers(users.filter(user => user._id !== userIdToDelete));
+                setShowModal(false);
+            }else{
+                console.log(data.message);
+            }
         } catch (error) {
-          console.log(error);
+          console.log(error.message);
         }
 }
 
